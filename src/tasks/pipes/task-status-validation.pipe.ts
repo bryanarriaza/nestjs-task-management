044f0a9b391e28f6aa95bc64dae60730fc02d664
Bryan Arriaza <bjarriaza.g.07@gmail.com>
2019-07-17 21:00:58 -0600

    Adding custom Pipe to validate status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../../model/task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+
+    private readonly allowedStatus = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE,
+    ];
+
+    transform(value: any) {
+        value = value.toUpperCase();
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} is an invalid status`);
+        }
+        return value;
+    }
+
+    private isStatusValid(status: any): any {
+        const idx = this.allowedStatus.indexOf(status);
+        return idx !== -1;
+    }
+
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from '../model/task.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter-dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -30,7 +31,7 @@ export class TasksController {
     }
 
     @Patch('/:id/status')
-    updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+    updateTaskStatus(@Param('id') id: string, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task {
         return this.tasksService.updateTaskStatus(id, status);
     }
 
