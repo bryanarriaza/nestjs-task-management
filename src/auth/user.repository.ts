ca35b499dc0921f93dd1ee58d028c8d499b4cc15
Bryan Arriaza <bjarriaza.g.07@gmail.com>
2019-07-20 14:18:37 -0600

    Adding security hash to save password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,7 +11,8 @@ export class UserRepository extends Repository<User> {
         const { username, password } = authCredentialsDto;
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
         try {
             await user.save();
         } catch (error) {
@@ -20,6 +22,10 @@ export class UserRepository extends Repository<User> {
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 
 }
