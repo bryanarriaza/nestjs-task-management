cc7568eb52109f1a301fbce0fb795e98a2e52296
Bryan Arriaza <bjarriaza.g.07@gmail.com>
2019-07-20 16:11:35 -0600

    Adding authorization guards to consume getAllTasks endpoint

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,12 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
 })
+
 export class TasksModule {}
